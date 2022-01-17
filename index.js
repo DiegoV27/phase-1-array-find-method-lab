ce6e9b17f8110e4c6a6bd89977e5f89c633b6366
Diego Valero <diego27jay@gmail.com>
2022-01-17 11:01:29 -0700

    commit tree 768f8ee14bf1709902c7afc965951fe02001039f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+const superbowlWin = (record) => {
+    let result = record.find((win) => {
+        return win.result =="W"
+    })
+    if (result != undefined) {
+        return result.year
+    }
+}
